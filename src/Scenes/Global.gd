922578dc026832e66f0aba70ccda163a85433369
Tim <elmtimmi@gmail.com>
2024-01-17 00:00:43 +0100

    impemented player_attributes by items

--- src/Scenes/Global.gd
+++ src/Scenes/Global.gd
@@ -4,18 +4,22 @@ extends Node2D
 var money = 0
 var round_counter = 0
 var backpack = []
+var player_attributes =  {} # e.g. {"angriffsgeschwindigkeit": 5, "schaden": 10, "verteidigung": 4}
 
 class Item:
 	var id: int
 	var title: String
 	var description: String
 	var cost: int
+	var bonus: Dictionary
+
 	
-	func _init(_id, _title, _description, _cost):
+	func _init(_id, _title, _description, _cost, _bonus):
 		id = _id
 		title = _title
 		description = _description
 		cost = _cost
+		bonus = _bonus
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
